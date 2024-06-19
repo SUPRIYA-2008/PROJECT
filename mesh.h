3eab48733df32eb322211aad823e88ee94af50e8
SUPRIYA-2008 <131749181+SUPRIYA-2008@users.noreply.github.com>
2024-06-19 16:45:23 +0530

    Create mesh.h
    added mesh file

---
+++ mesh.h
@@ -0,0 +1,127 @@
+#pragma once
+
+#include<vector>
+
+#include<glm/glm.hpp>
+#include <string>
+#include <glad/glad.h>
+
+
+
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
+
+struct Texture {
+	unsigned int id;
+	string type;
+};
+
+
+class Mesh
+{
+public:
+	// mesh Data
+	std::vector<Vertex>       vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture>      textures;
+	unsigned int VAO;
+
+
+
+	//The constructor takes three parameters
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
+	{
+		//constructor parameters
+		//The this-> keyword is used to distinguish the member variables from the constructor parameters
+		This->vertices = vertices;
+		This->indices = indices;
+		This->textures = textures;
+	}
+
+
+	// This method will be used to draw the mesh using it is done by binding the VAO
+	// Rendering the mesh
+
+	//Before rendering the mesh, we first want to bind the appropriate textures before calling glDrawElements
+	//we don't know from the start how many (if any) textures the mesh has and what type they may have
+	//each diffuse texture is named texture_diffuseN, and each specular texture should be named texture_specularN where N is any number ranging from 1 to the maximum number of texture samplers allowed
+	void Draw(Shader& shader) {
+		// bind appropriate textures
+		unsigned int diffuseNr = 1;
+		unsigned int specularNr = 1;
+		unsigned int normalNr = 1;
+		unsigned int heightNr = 1;
+		for (unsigned int i = 0; i < textures.size(); i++)
+		{
+			glActiveTexture(GL_TEXTURE0 + i); // active proper texture unit before binding
+			// retrieve texture number (the N in diffuse_textureN)
+			string number;
+			string name = textures[i].type;
+			if (name == "texture_diffuse")
+				number = std::to_string(diffuseNr++);
+			else if (name == "texture_specular")
+				number = std::to_string(specularNr++); // transfer unsigned int to string
+			else if (name == "texture_normal")
+				number = std::to_string(normalNr++); // transfer unsigned int to string
+			else if (name == "texture_height")
+				number = std::to_string(heightNr++); // transfer unsigned int to string
+
+			// now set the sampler to the correct texture unit
+			glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i);
+			// and finally bind the texture
+			glBindTexture(GL_TEXTURE_2D, textures[i].id);
+		}
+
+		// draw mesh
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		// always good practice to set everything back to defaults once configured.
+		glActiveTexture(GL_TEXTURE0);
+	
+	
+	}
+
+
+private:
+
+	unsigned int VAO, VBO, EBO;
+
+
+	// initializes all the buffer objects/arrays
+	void setupMesh()
+	{
+		glGenVertexArrays(1,&VAO);
+		glBindVertexArray(VAO);
+
+
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+		
+	    glGenBuffers(1, &EBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size(), &indices[0], GL_STATIC_DRAW);
+
+		//vertex positions
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+
+		glBindVertexArray(0);
+		
+
+
+	}
+};
+
+//G
